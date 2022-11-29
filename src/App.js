77c33d8cc6b3cebf57759420e5c8cee6c0056041
INT\DanielXabier.Lopez <danielxabier.lopez@solera.com>
2022-11-29 12:38:21 +0100

    todo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from "./logo.svg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "./components/homePage/homePage";
+import "./components/loginPage/loginPage";
+import "./components/FlightsPage/FlightPage";
+import "./components/FlightsPage/FlightForm/FlightForm";
+import "./components/MyFlights/FlightsBooked";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<loginPage />} />
-        <Route path="/Home" element={<homePage />} />
+        <Route path="/login" element={<loginPage />} />
+        <Route path="/home" element={<homePage />} />
       </Routes>
     </BrowserRouter>
   );
