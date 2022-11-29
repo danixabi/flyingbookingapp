3da8e5008226b3c19f2758a41efe08d8c715ef2e
INT\DanielXabier.Lopez <danielxabier.lopez@solera.com>
2022-11-29 15:50:44 +0100

    Routing arreglado pero no implementado en botones

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import "./components/homePage/homePage";
-import "./components/loginPage/loginPage";
-import "./components/FlightsPage/FlightPage";
-import "./components/FlightsPage/FlightForm/FlightForm";
-import "./components/MyFlights/FlightsBooked";
+import HomePage from "./components/homePage/HomePage";
+import LoginPage from "./components/loginPage/LoginPage";
+import FlightPage from "./components/FlightsPage/FlightPage";
+import FlightForm from "./components/FlightsPage/FlightForm/FlightForm";
+import MyFlights from "./components/MyFlights/FlightsBooked";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<loginPage />} />
-        <Route path="/home" element={<homePage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/Home" element={<HomePage />} />
+        <Route path="/Flights" element={<FlightPage />} />
+        <Route path="/Flights/Form" elemnt={<FlightForm />} />
+        <Route path="/MyFlights" element={<MyFlights />} />
       </Routes>
     </BrowserRouter>
   );
